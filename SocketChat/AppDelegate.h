edf5a199889c77d02f00b856ef550a20ee03e8cd
Manuel Menzella <manuel.menzella@gmail.com>
2014-07-02 17:55:52 -0300

    Add Xcode project

---
+++ SocketChat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SocketChat
+//
+//  Created by Manuel Menzella on 7/2/14.
+//  Copyright (c) 2014 Manuel Menzella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
